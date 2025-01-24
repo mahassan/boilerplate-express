5ba6e1ae3693a7222b4dd3f4a2cfecb590bfcc1f
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-01-24 19:51:28 +0000

    response to the root file

--- myApp.js
+++ myApp.js
@@ -1,39 +1,9 @@
 let express = require('express');
 let app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req, res){
+    res.send("Hey");
+    res.sendFile(__dirname + "/views/index.html");
+})
 
  module.exports = app;
