93ac807bbb2b256bb475f4c5951d7063038ea4c8
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-10-21 11:13:25 +0100

    .env and ternary opetator

--- myApp.js
+++ myApp.js
@@ -9,7 +9,7 @@ app.use('/public',express.static(__dirname+ '/public'));
 
 app.get('/json', function(req, res){
     res.json({
-        "message": process.env.MESSAGE_STYLE === 'uppercase'? 'HELLO JSON': 'hello'
+        "message": process.env.MESSAGE_STYLE === 'uppercase'? 'HELLO JSON': 'hello json'
     })
 })
 
