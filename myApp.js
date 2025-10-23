bc4d8fa0786d3614be0b8ffc84ab399d22026de1
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-10-23 14:17:47 +0100

    latest

--- myApp.js
+++ myApp.js
@@ -9,8 +9,13 @@ app.use('/public',express.static(__dirname+ '/public'));
 
 app.get('/json', function(req, res){
     res.json({
-        "message": process.env.MESSAGE_STYLE === 'uppercase'? 'HELLO JSON': 'hello json'
+        "message": process.env.MESSAGE_STYLE === 'uppercase'? 'HELLO JSON': 'Hello json'
     })
+})
+
+app.get('/*', function(req, res, next){
+    console.log(`${req.method} - ${req.path}${req.ip}`);
+    next();
 })
 
 console.log('Hello Express');
